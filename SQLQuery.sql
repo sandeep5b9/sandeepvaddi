9dfc8fb2582334b111db15750e3c7570f5a9e64f
sandeep5b9 <74094481+sandeep5b9@users.noreply.github.com>
2020-11-11 13:46:48 +0530

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -11,4 +11,15 @@ create table `test`.`loaninfo` (
   `address` varchar(250) not null,
   `email` varchar(100) not null,
   `mobile` varchar(10) not null,
+  `term` varchar(15) not null,
+  primary key (`loanid`));
+  
+  create table `test`.`adminloaninfo` (
+  `loanid` int not null,
+  `amountsanctioned` int not null,
+  `loanterm` int not null,
+  `paymentstartdate` date not null,
+  `paymentenddate` date not null,
+  `termpayment` varchar(45) not null,
+  `monthlypayment` varchar(45) not null,
   primary key (`loanid`));
